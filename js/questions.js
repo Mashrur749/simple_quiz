ea19c720cb9e1eda2016b351317985ad9d8de088
Musaddiqur Rahman <mashrur749@gmail.com>
2018-03-15 10:58:43 +0000

    finished

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// Questions for the quiz app
+
+var questions = [{
+    "question": "Which American inventor is generally given credit for the invention of the lightning rod?",
+    "option1": "Nikola Tesla",
+    "option2": "George Washington",
+    "option3": "Benjamin Franklin",
+    "option4": "Elijah McCoy",
+    "answer": "3"
+}, {
+    "question": "Jimmy Carter was the first U.S. president born in a what?",
+    "option1": "Air Plane",
+    "option2": "Hospital",
+    "option3": "Submarine",
+    "option4": "Island",
+    "answer": "2"
+}, {
+    "question": "In Disney's 'The Little Mermaid' what is the name of the human that Ariel falls in love with?",
+    "option1": "King George",
+    "option2": "Barry White",
+    "option3": "Olaf The Snowman",
+    "option4": "Prince Eric",
+    "answer": "4"
+}, {
+    "question": "What is the name of the animated science fiction comedy-drama film released in 2002 about a Hawaiin girl and her unusual pet?",
+    "option1": "Lilo & Stitch",
+    "option2": "Moana",
+    "option3": "How To Train Your Dragon",
+    "option4": "Coraline",
+    "answer": "1"
+}, {
+    "question": "How many mother sauces are there in classical French cuisine?",
+    "option1": "Twenty",
+    "option2": "Two",
+    "option3": "Five",
+    "option4": "Nine",
+    "answer": "3"
+}, {
+    "question": "The original Starbucks was established in 1971 in what U.S. city?",
+    "option1": "Los Angeles, California",
+    "option2": "Orlando, Florida",
+    "option3": "Seattle, Washington",
+    "option4": "Dallas, Texas",
+    "answer": "3"
+}, {
+    "question": "What do the letters in the acronym CD-ROM stand for?",
+    "option1": "Compact Disk Recorded On Memory",
+    "option2": "Compact Disk Read Only Memory",
+    "option3": "Who Cares We Have DvD's Now!",
+    "option4": "Compact Disk Ready On Memory",
+    "answer": "2"
+}, {
+    "question": "In 1975 an engineer created the first electronic camera while working for what company?",
+    "option1": "Microsoft",
+    "option2": "IBM",
+    "option3": "Polaroid",
+    "option4": "Kodak",
+    "answer": "4"
+}, {
+    "question": "Nintendo is a consumer electronics and video game company founded in what country?",
+    "option1": "China",
+    "option2": "South Korea",
+    "option3": "Japan",
+    "option4": "United States",
+    "answer": "3"
+}, {
+    "question": "Which planet in our solar system has an axis that is titled by 98 degrees?",
+    "option1": "Uranus",
+    "option2": "Saturn",
+    "option3": "Mars",
+    "option4": "Mercury",
+    "answer": "1"
+}];
---
+++ js/quiz.js
@@ -0,0 +1,47 @@
+var quizContainer   = document.querySelector("#quizContainer"),
+	question 		= document.querySelector("#question"),
+	opt1 			= document.querySelector("#opt1"),
+	opt2		 	= document.querySelector("#opt2"),
+	opt3 			= document.querySelector("#opt3"),
+	opt4 			= document.querySelector("#opt4"),
+	nextQ			= document.querySelector("#nextQ"),
+	result			= document.querySelector("#result");
+
+result.style.display = "none";
+var currQuestion = 0,
+	score 		 = 0,
+	totQustion   = questions.length;
+	
+function loadQ(Qindex){
+    question.textContent    = (Qindex+1) + ". " + questions[Qindex].question;
+    opt1.textContent        = questions[Qindex].option1;
+    opt2.textContent        = questions[Qindex].option2;
+    opt3.textContent        = questions[Qindex].option3;
+    opt4.textContent        = questions[Qindex].option4;
+}
+
+nextQ.onclick = function(){
+    var selectedOption  = document.querySelector("input[type=radio]:checked");
+    var corrAnswer      = questions[currQuestion].answer;
+    
+    if(!selectedOption){
+        alert("Please Select Your Answer");
+    }
+    
+    if(selectedOption.value == corrAnswer){
+        score += 10;
+    }
+    selectedOption.checked = false;
+    currQuestion++;
+    if(currQuestion == totQustion){
+        quizContainer.style.display = "none";
+        result.style.display = "block";
+        result.innerHTML = "Your total score is " + score + " <br>Out of 100";
+        return;
+    }
+    loadQ(currQuestion);
+    
+}
+
+loadQ(currQuestion);
+
